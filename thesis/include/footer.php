eb884723625c9a7a45c2a544a0395e43c3dadf40
Noemitaneo <62083474+Noemitaneo@users.noreply.github.com>
2021-09-20 12:52:51 +0800

    task1 index

---
+++ thesis/include/footer.php
@@ -0,0 +1,28 @@
+ <!----------- Footer ------------>
+ <footer class="footer-bs">
+     <div class="row">
+         <div class="col-md-4 footer-brand img-fluid">
+             <img src="asset/img/logo1.png" style="width: 18rem; height:auto; margin-bottom: 15px;">
+             <li class="location">Brgy. Don Felipe Larrazabal,
+                 Ormoc City, Philippines 6541</li>
+             <li class="location">© 2021 All rights reserved</li>
+         </div>
+         <div class="col-md-3 footer-nav ">
+             <div class="col-md-8">
+                 <ul class="list">
+                     <li><a href="../page/terms.php">Terms & Condition</a></li>
+                     <li><a href="../page/privacy.php">Privacy Policy</a></li>
+                 </ul>
+             </div>
+         </div>
+         <div class="col-md-3 footer-ns">
+             <h4>Contact Us</h4>
+             <li><i class="fas fa-phone-alt"></i> 0927 286 0251</li>
+             <li><i class="fas fa-envelope"></i> evsuoccsports@gmail.com</li>
+             <li><i class="fab fa-facebook"></i> facebook.com/evsuoccsports</li>
+         </div>
+         <div class="col-md-2 footer-camp img-fluid">
+             <img src="asset/img/evsulympicsLogo.png" style="width: 12rem; height:auto; margin-bottom: 15px;">
+         </div>
+     </div>
+ </footer>
---
+++ thesis/include/head.php
@@ -0,0 +1,23 @@
+ <meta charset="utf-8">
+ <meta name="viewport" content="width=device-width, initial-scale=1">
+ <meta name="description" content="">
+ <meta name="author" content="Mark Otto, Jacob Thornton, and Bootstrap contributors">
+ <meta name="generator" content="Hugo 0.88.1">
+ <title>EVSU-OCC Athletes Management and Information</title>
+ <link rel="icon" href="asset/img/evsulogo.png">
+
+ <link rel="canonical" href="https://getbootstrap.com/docs/5.1/examples/carousel/">
+ <!-- Bootstrap core CSS -->
+ <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.1.1/dist/css/bootstrap.min.css" rel="stylesheet"
+     integrity="sha384-F3w7mX95PdgyTmZZMECAngseQB83DfGTowi0iMjiWaeVhAn4FJkqJByhZMI3AhiU" crossorigin="anonymous">
+ <link rel="stylesheet" href="asset/css/style.css">
+ <!-- Custom styles for this template -->
+ <link href="asset/css/carousel.css" rel="stylesheet">
+ <!-- Font Awesome -->
+ <link rel="stylesheet" href="https://use.fontawesome.com/releases/v5.15.4/css/all.css"
+     integrity="sha384-DyZ88mC6Up2uqS4h/KRgHuoeGwBcD4Ng9SiP4dIRy0EXTlnuz47vAwmeGwVChigm" crossorigin="anonymous">
+
+ <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.1.1/dist/js/bootstrap.bundle.min.js"
+     integrity="sha384-/bQdsTh/da6pkI1MST/rWKFNjaCP5gBSY4sEBT38Q/9RBh9AH40zEOg7Hlq2THRZ" crossorigin="anonymous">
+ </script>
+ <script src="asset/js/all.js"></script>
